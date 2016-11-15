6814e02f0c1d29afcf5886fa7f67f473c4139ea8
MIKAYLA W <mikayla.whiteaker@gmail.com>
2016-11-14 19:13:27 -0800

    Flushed out MazeTile

--- app/src/main/java/edu/up/cs301/Labyrinth/MazeTile.java
+++ app/src/main/java/edu/up/cs301/Labyrinth/MazeTile.java
@@ -11,19 +11,49 @@ import java.util.ArrayList;
  */
 
 public class MazeTile {
-
+    //this is for dummy data
     public int val;
-    public ArrayList<Integer> occupiedBy = new ArrayList<Integer>(1);
 
-    public MazeTile( int i )
+    //the players currently on this tile
+    public ArrayList<Integer> occupiedBy = new ArrayList<Integer>(4);
+
+    //S(straight) T or L
+    private char type;
+
+    //n = 0-3 n*90 = the degrees from noraml
+    private int orientation;
+
+    //the treasure
+    private LabTSymbol treasureSymbol;
+
+    /*
+     * Welcome to this wonderful constructor takes the type and Symbol
+     */
+    public MazeTile(char theType, LabTSymbol symbol)
     {
-        val = i;
+        type = theType;
+        treasureSymbol = symbol;
+        orientation = 0;
     }
+
+    //getters for all the attributtes
+
+    public char getType(){ return type; }
+
+    public int getOrientation(){ return orientation; }
+
+    public LabTSymbol getTreasureSymbol(){ return treasureSymbol; }
+
+    public ArrayList<Integer> getOccupiedBy(){ return occupiedBy; }
+
+    //setters for occupiedBy
 
     public void addPlayer(int player)
     {
         occupiedBy.add(player);
     }
+
+    public void removePlayer(int player){ occupiedBy.remove((Integer)player); }
 
 
 
