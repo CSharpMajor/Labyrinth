c227a5c09ee232698ba4cc88a937278646581957
nakis <kister19@up.edu>
2016-11-20 11:34:38 -0800

    LabComputerPlayer completed.

--- app/src/main/java/edu/up/cs301/Labyrinth/LabComputerPlayer.java
+++ app/src/main/java/edu/up/cs301/Labyrinth/LabComputerPlayer.java
@@ -20,10 +20,8 @@ import edu.up.cs301.game.infoMsg.NotYourTurnInfo;
  * @author Mikayla Whiteaker
  * @version Nov 2016, preAlpha
  */
-public class LabComputerPlayer extends GameComputerPlayer
-{
-    public LabComputerPlayer()
-    {
+public class LabComputerPlayer extends GameComputerPlayer {
+    public LabComputerPlayer() {
         super("hi");
     }
 
@@ -33,56 +31,39 @@ public class LabComputerPlayer extends GameComputerPlayer
         int yCordMaze;
         int xCordPiece;
         int yCordPiece;
-        if(info instanceof LabGameState){
+        Random rand = new Random();
+
+        if (info instanceof LabGameState) {
             LabGameState myGameState = (LabGameState) info;
-            if(this.playerNum != myGameState.getTurnID()){
+            if (this.playerNum != myGameState.getTurnID()) {
                 return;
             }
-            else{
-                if(myGameState.hasMovedMaze()){
-                    //move piece
-                    Random rand = new Random();
-                    Log.i("Computer", "moved piece");
-                    //random value between 1-8
-                    LabMovePieceAction action = new LabMovePieceAction(this, rand.nextInt(8)+1, rand.nextInt(8)+1);
+            else {
+                if (myGameState.hasMovedMaze()) {
+                    //Pick a random spot to move the player
+                    xCordPiece = rand.nextInt(6) + 1;
+                    yCordPiece = rand.nextInt(6) + 1;
+                    //Delay to make it look like the player is thinking
+                    sleep(3000);
+                    //Then send the move piece action to the local game
+                    game.sendAction(new LabMovePieceAction(this, xCordPiece, yCordPiece));
                 }
-                else{
-                    //move maze
-                    Random rand = new Random();
-                    Log.i("Computer", "moved maze");
-                    //LabMoveMazeAction action = new LabMoveMazeAction(this, rand.nextInt());
-                    //game.sendAction(action);
+                else {
+                    xCordMaze = rand.nextInt(8);
+                    yCordMaze = rand.nextInt(8);
+                    //If the random spot is not even, pick again
+                    while (xCordMaze % 2 != 0) {
+                        xCordMaze = rand.nextInt(8);
+                    }
+                    while (yCordMaze % 2 != 0) {
+                        yCordMaze = rand.nextInt(8);
+                    }
+                    //Delay to make it look like the player is thinking
+                    sleep(3000);
+                    //Then send the move maze action to the local game
+                    game.sendAction(new LabMoveMazeAction(this, xCordMaze, yCordMaze));
                 }
-
-        //If it isn't the computer's turn, ignore
-        if(info instanceof NotYourTurnInfo){
-            return;
+            }
         }
-
-        //Pick a random spot to place the extra tile
-        Random rand = new Random();
-        xCordMaze = rand.nextInt(8);
-        yCordMaze = rand.nextInt(8);
-        //If the random spot is not even, pick again
-        while(xCordMaze % 2 != 0)
-        {
-            xCordMaze = rand.nextInt(8);
-        }
-        while(yCordMaze % 2 != 0)
-        {
-            yCordMaze = rand.nextInt(8);
-        }
-        //Delay to make it look like the player is thinking
-        sleep(3000);
-        //Then send the move maze action to the local game
-        game.sendAction(new LabMoveMazeAction(this, xCordMaze, yCordMaze));
-
-        //Pick a random spot to move the player
-        xCordPiece = rand.nextInt(6) + 1;
-        yCordPiece = rand.nextInt(6) + 1;
-        //Delay to make it look like the player is thinking
-        sleep(3000);
-        //Then send the move piece action to the local game
-        game.sendAction(new LabMovePieceAction(this, xCordPiece, yCordPiece));
     }
 }
