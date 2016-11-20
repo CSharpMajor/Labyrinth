83c980e83b892909ea8514f237a76d176ce0f406
nakis <kister19@up.edu>
2016-11-20 11:27:48 -0800

    LabComputerPlayer completed.

--- app/src/main/java/edu/up/cs301/Labyrinth/LabComputerPlayer.java
+++ app/src/main/java/edu/up/cs301/Labyrinth/LabComputerPlayer.java
@@ -1,5 +1,7 @@
 package edu.up.cs301.Labyrinth;
 
+import java.util.Random;
+
 import edu.up.cs301.game.GameComputerPlayer;
 import edu.up.cs301.game.infoMsg.GameInfo;
 import edu.up.cs301.game.infoMsg.NotYourTurnInfo;
@@ -22,6 +24,41 @@ public class LabComputerPlayer extends GameComputerPlayer
     }
 
     protected void receiveInfo(GameInfo info) {
+        //Coordinates for the player to send to the local game
+        int xCordMaze;
+        int yCordMaze;
+        int xCordPiece;
+        int yCordPiece;
 
+        //If it isn't the computer's turn, ignore
+        if(info instanceof NotYourTurnInfo){
+            return;
+        }
+
+        //Pick a random spot to place the extra tile
+        Random rand = new Random();
+        xCordMaze = rand.nextInt(8);
+        yCordMaze = rand.nextInt(8);
+        //If the random spot is not even, pick again
+        while(xCordMaze % 2 != 0)
+        {
+            xCordMaze = rand.nextInt(8);
+        }
+        while(yCordMaze % 2 != 0)
+        {
+            yCordMaze = rand.nextInt(8);
+        }
+        //Delay to make it look like the player is thinking
+        sleep(3000);
+        //Then send the move maze action to the local game
+        game.sendAction(new LabMoveMazeAction(this, xCordMaze, yCordMaze));
+
+        //Pick a random spot to move the player
+        xCordPiece = rand.nextInt(6) + 1;
+        yCordPiece = rand.nextInt(6) + 1;
+        //Delay to make it look like the player is thinking
+        sleep(3000);
+        //Then send the move piece action to the local game
+        game.sendAction(new LabMovePieceAction(this, xCordPiece, yCordPiece));
     }
 }
---
+++ app/src/test/java/edu/up/cs301/Labyrinth/LabComputerPlayerTest.java
@@ -0,0 +1,16 @@
+package edu.up.cs301.Labyrinth;
+
+import static org.junit.Assert.*;
+
+/**
+ * Testing the LabComputerPlayer
+ *
+ * @author Chole Kuhar
+ * @author Liz Frick
+ * @author Nicole Kister
+ * @author Mikayla Whiteaker
+ * @version Nov 2016, preAlpha
+ */
+public class LabComputerPlayerTest {
+
+}
