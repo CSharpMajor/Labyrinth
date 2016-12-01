70be3f96e4a6f480f55785370fc406caa97e1c57
MIKAYLA W <mikayla.whiteaker@gmail.com>
2016-11-30 17:11:25 -0800

    added rotate extra tile action class

---
+++ app/src/main/java/edu/up/cs301/Labyrinth/LabRotateExtraTileAction.java
@@ -0,0 +1,19 @@
+package edu.up.cs301.Labyrinth;
+
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
+/**
+ * Created by mikayla on 11/30/2016.
+ */
+
+public class LabRotateExtraTileAction extends GameAction {
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public LabRotateExtraTileAction(GamePlayer player) {
+        super(player);
+    }
+}
